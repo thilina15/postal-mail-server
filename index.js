3ad9794707f4a2b633d766cc6710058f1afd8a27
thilina15 <thibbatz@gmail.com>
2022-09-05 18:10:14 +0530

    initial test files

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+
+
+app.post('/mail',(req,res)=>{
+    console.log('new email is comming------>>>');
+    console.log(req.body);
+    console.log('end of email---------------------------------------------');
+    res.status(200).send('done')
+})
+
+app.get('/',(req,res)=>{
+    res.send('server is running----------!')
+})
+
+
+app.listen(4000,()=>{
+    console.log("server is running on port 4000");
+})
